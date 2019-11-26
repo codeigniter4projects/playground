470da919ade84c10aff54f6fc39d6da396489af7
Matt Gatner <mgatner@tattersoftware.com>
2019-11-26 21:30:36 +0000

    First pass at tests

---
+++ tests/database/HeroTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class HeroTest extends ProjectTests\Support\DatabaseTestCase
+{
+	public function setUp(): void
+	{
+		parent::setUp();
+	}
+
+	public function testHasHero()
+	{
+		$hero = [
+			'name'    => 'Ursula Frost',
+			'pronoun' => 'she',
+		];
+		
+		$this->seeInDatabase('heroes', $hero);
+	}
+
+	public function testModelFindHero()
+	{
+		$model = new \App\Models\HeroModel();
+
+		$hero = $model->find(1);
+
+		$this->assertIsObject($hero);
+		$this->assertEquals('Hallam Swales', $hero->name);
+	}
+}
