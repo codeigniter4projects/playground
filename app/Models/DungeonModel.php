c169e0e2ca583cb2e6857d64f2a97b5837172c89
Lonnie Ezell <lonnieje@gmail.com>
2019-11-29 00:00:26 -0600

    New Dungeon model, entitity, and added to seeder.

---
+++ app/Models/DungeonModel.php
@@ -0,0 +1,28 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class DungeonModel extends Model
+{
+	protected $table      = 'dungeons';
+	protected $primaryKey = 'id';
+
+	// We are specifying an Entity class to return
+    // here instead of a simple array or object.
+    // You don't have to extend CI's Entity class, though,
+    // any class can be used.
+	protected $returnType = 'App\Entities\Dungeon';
+	protected $useSoftDeletes = false;
+
+	protected $allowedFields = ['name', 'difficulty', 'capacity'];
+
+	protected $useTimestamps = true;
+
+	protected $validationRules    = [
+		'name'  => 'required|min_length[2]',
+		'difficulty' => 'is_natural_no_zero',
+		'capacity' => 'is_natural_no_zero',
+	];
+	protected $validationMessages = [];
+	protected $skipValidation     = false;
+}
