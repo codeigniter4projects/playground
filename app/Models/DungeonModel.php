f0f76b40a41c49bbc80ac49ef053b494326ff847
MGatner <mgatner@icloud.com>
2020-07-13 01:47:53 +0000

    Add more database, start faker

--- app/Models/DungeonModel.php
+++ app/Models/DungeonModel.php
@@ -8,9 +8,9 @@ class DungeonModel extends Model
 	protected $primaryKey = 'id';
 
 	// We are specifying an Entity class to return
-    // here instead of a simple array or object.
-    // You don't have to extend CI's Entity class, though,
-    // any class can be used.
+	// here instead of a simple array or object.
+	// You don't have to extend CI's Entity class, though,
+	// any class can be used.
 	protected $returnType = 'App\Entities\Dungeon';
 	protected $useSoftDeletes = false;
 
@@ -19,10 +19,22 @@ class DungeonModel extends Model
 	protected $useTimestamps = true;
 
 	protected $validationRules    = [
-		'name'  => 'required|min_length[2]',
+		'name'       => 'required|min_length[2]',
 		'difficulty' => 'is_natural_no_zero',
-		'capacity' => 'is_natural_no_zero',
+		'capacity'   => 'is_natural_no_zero',
 	];
 	protected $validationMessages = [];
 	protected $skipValidation     = false;
+
+	/**
+	 * Help! Dungeons have monsters in them, but we have no way of knowing
+	 * which monsters are part of a dungeon. Can you write it?
+	 *
+	 * @param int $dungeonId
+	 *
+	 * @return array
+	 */
+	//public function getMonstersForDungeon(int $dungeonId): array
+	//{
+	//}
 }
---
+++ tests/_support/Fakers/MonsterFaker.php
@@ -0,0 +1,38 @@
+<?php namespace Tests\Support\Fakers;
+
+use App\Entities\Monster;
+use App\Models\MonsterModel;
+use CodeIgniter\Test\Interfaces\FabricatorModel;
+use Faker\Generator;
+
+/**
+ * Fabricator is a part of the framework's core test utilities. It allows
+ * developers to generate random data with predefined formats for testing.
+ * By extending our Model and adding the `fake()` method we are telling
+ * Fabricator that it can use this extension to generate rows for `monsters`.
+ */
+class MonsterFaker extends MonsterModel implements FabricatorModel
+{
+	/**
+	 * Faked data for Fabricator.
+	 *
+	 * @param Generator $faker
+	 *
+	 * @return Monster
+	 */
+	public function fake(Generator &$faker): Monster
+	{
+		return new Monster([
+			'name'       => $faker->lastName,
+			'health'     => rand(1, 50),
+
+			/**
+			 * This is a special function of Fabricator that means "get how many items
+			 * have been generated for this table so far". It is handy for faking
+			 * related items, like the number of possible dungeons. But if none have
+			 * been made we will just pick a number.
+			 */
+			'dungeon_id' => rand(1, Fabricator::getCount('dungeons') ?: 3),
+		]);
+	}
+}
