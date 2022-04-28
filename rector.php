6d370fa9341cbc670adcdc4c4533d16cab1bdae2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-28 19:02:43 +0700

    Update to use RectorConfig

--- rector.php
+++ rector.php
@@ -1,32 +1,25 @@
 <?php
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
 use Rector\TypeDeclaration\Rector\Param\ParamTypeFromStrictTypedPropertyRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::PHP_73);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
-
-    $parameters = $containerConfigurator->parameters();
-
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([SetList::DEAD_CODE, SetList::PHP_73, SetList::TYPE_DECLARATION, SetList::TYPE_DECLARATION_STRICT]);
     // paths to refactor; solid alternative to CLI arguments
-    $parameters->set(Option::PATHS, [__DIR__ . '/app']);
+    $rectorConfig->paths([__DIR__ . '/app']);
 
     // do you need to include constants, class aliases or custom autoloader? files listed will be executed
-    $parameters->set(Option::BOOTSTRAP_FILES, [
+    $rectorConfig->bootstrapFiles([
         __DIR__ . '/vendor/codeigniter4/codeigniter4/system/Test/bootstrap.php',
     ]);
 
     // is there a file you need to skip?
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->skip([
         __DIR__ . '/app/Controllers/BaseController.php',
         __DIR__ . '/app/Config',
         __DIR__ . '/app/Views',
@@ -41,6 +34,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     // auto import fully qualified class names
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_73);
+    $rectorConfig->importNames();
+    $rectorConfig->phpVersion(PhpVersion::PHP_73);
 };
