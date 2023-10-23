2e5ff62ad125510a188d5c65bdc3593e4ee00e30
kenjis <kenji.uui@gmail.com>
2023-10-23 17:50:13 +0900

    chore: igonre 'php'
    I don't know why this is needed.

--- composer-unused.php
+++ composer-unused.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
 use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
-use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
-use Webmozart\Glob\Glob;
 
 return static function (Configuration $config): Configuration {
     return $config
-        ->addNamedFilter(NamedFilter::fromString('codeigniter4/framework'));
+        ->addNamedFilter(NamedFilter::fromString('codeigniter4/framework'))
+        ->addNamedFilter(NamedFilter::fromString('php'));
 };
